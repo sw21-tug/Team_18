ec300a7f135ce9203a397de2a5438c4fc3de12fa
Kilian Weisl <weisl@student.tugraz.at>
2021-05-04 12:10:07 +0200

    SAUP-7 implemented error handling and redirect on success [WE, WK]

--- SaveUp/app/src/main/java/com/example/saveup/RegistrationActivity.kt
+++ SaveUp/app/src/main/java/com/example/saveup/RegistrationActivity.kt
@@ -2,6 +2,7 @@ package com.example.saveup
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.EditText
@@ -18,11 +19,21 @@ class RegistrationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_registration)
     }
 
+    fun String.isEmailValid(): Boolean {
+        return !TextUtils.isEmpty(this) && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
+
     fun register(view: View) {
         val prename = findViewById<EditText>(R.id.registration_prename).text.toString()
         val lastname = findViewById<EditText>(R.id.registration_lastname).text.toString()
         val email = findViewById<EditText>(R.id.registration_email).text.toString()
         val password = findViewById<EditText>(R.id.registration_password).text.toString()
+
+        if(!email.isEmailValid())
+        {
+            Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT).show()
+            return
+        }
 
         val queue = Volley.newRequestQueue(this)
         val url = "https://saveup.weisl.cc/users"
@@ -33,10 +44,15 @@ class RegistrationActivity : AppCompatActivity() {
                 Response.Listener { response ->
                     // response
                     val strResp = response.toString()
+                    Toast.makeText(this, "Registration successful!", Toast.LENGTH_SHORT).show()
 
                     Log.d("API", strResp)
+
+                    val intent = Intent (this, LoginActivity::class.java)
+                    startActivity(intent)
                 },
                 Response.ErrorListener { error ->
+                    Toast.makeText(this, "Error. Please check input and internet connection.", Toast.LENGTH_SHORT).show()
                     Log.d("API", "error => $error")
                 }
             ){
