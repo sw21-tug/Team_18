6c52e2bd8d837e6f547bcea634ab8bca8b0240ea
Daniel Fürpaß <fuerpass@student.tugraz.at>
2021-05-12 11:26:05 +0200

    SAUP-17 add database login [FD, WK]

--- SaveUp/app/src/main/java/com/example/saveup/LoginActivity.kt
+++ SaveUp/app/src/main/java/com/example/saveup/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.example.saveup
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.nio.charset.Charset
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,15 +23,35 @@ class LoginActivity : AppCompatActivity() {
         val email = findViewById<EditText>(R.id.login_email).text.toString()
         val password = findViewById<EditText>(R.id.login_password).text.toString()
 
-        if (email == "root@root.at" && password == "root") {
-            val sharedPref = getSharedPreferences("User", Context.MODE_PRIVATE).edit()
-            sharedPref.putString("user_token", "asdfasdfasdfasdfasfd")
-            sharedPref.apply()
-            val intent = Intent (this, ProfileActivity::class.java)
-            startActivity(intent)
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://saveup.weisl.cc/auth"
 
-        } else {
-            Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show()
-        }
+        val requestBody = "email=" + email + "&pw=" + password
+        val stringReq : StringRequest =
+                object : StringRequest(Method.POST, url,
+                        Response.Listener { response ->
+                            // response
+                            val strResp = response.toString()
+                            //Toast.makeText(this, "Registration successful!", Toast.LENGTH_SHORT).show()
+
+                            Log.d("token", strResp)
+
+                            val sharedPref = getSharedPreferences("User", Context.MODE_PRIVATE).edit()
+                            sharedPref.putString("user_token", strResp)
+                            sharedPref.apply()
+                            val intent = Intent (this, ProfileActivity::class.java)
+                            startActivity(intent)
+                        },
+                        Response.ErrorListener { error ->
+                            Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show()
+                            Log.d("token", "error => $error")
+                        }
+                ){
+                    override fun getBody(): ByteArray {
+                        return requestBody.toByteArray(Charset.defaultCharset())
+                    }
+                }
+        queue.add(stringReq)
+
     }
 }
