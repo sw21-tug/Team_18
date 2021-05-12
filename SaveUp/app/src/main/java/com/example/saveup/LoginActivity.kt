d38e90f5b137c4f4bf99222f8808622cc4b7acdb
Daniel Fürpaß <fuerpass@student.tugraz.at>
2021-05-12 10:33:52 +0200

    SAUP-17 add preference for token [FD, WK]

--- SaveUp/app/src/main/java/com/example/saveup/LoginActivity.kt
+++ SaveUp/app/src/main/java/com/example/saveup/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.saveup
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -18,8 +19,12 @@ class LoginActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.login_password).text.toString()
 
         if (email == "root@root.at" && password == "root") {
+            val sharedPref = getSharedPreferences("User", Context.MODE_PRIVATE).edit()
+            sharedPref.putString("user_token", "asdfasdfasdfasdfasfd")
+            sharedPref.apply()
             val intent = Intent (this, ProfileActivity::class.java)
             startActivity(intent)
+
         } else {
             Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show()
         }
