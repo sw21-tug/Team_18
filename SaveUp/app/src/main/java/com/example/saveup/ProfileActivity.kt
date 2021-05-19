a94d2ed6fc23cae97c30fbbc8d19e726ab573efa
Kilian Weisl <weisl@student.tugraz.at>
2021-05-19 11:31:12 +0200

    SAUP-20 change profile activity [WK, TJ]

--- SaveUp/app/src/main/java/com/example/saveup/ProfileActivity.kt
+++ SaveUp/app/src/main/java/com/example/saveup/ProfileActivity.kt
@@ -5,16 +5,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.content_profile.*
 
 class ProfileActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
         val sharedPref = getSharedPreferences("User", Context.MODE_PRIVATE)
         val tokenToGet: String = sharedPref.getString("user_token", "")!!
         Log.d("TOKEN", tokenToGet)
+
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
         val fab: View = findViewById(R.id.form_button)
         fab.setOnClickListener {
