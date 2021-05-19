f2c733f5c272076d01d988d0a646a5a73841706a
Erwin Wagner <wagner@student.tugraz.at>
2021-05-19 09:30:09 +0200

    SAUP-10 changes to RecyclerView list layout [WE, TJ]

--- SaveUp/app/src/main/java/com/example/saveup/ui/form/ListAdapter.kt
+++ SaveUp/app/src/main/java/com/example/saveup/ui/form/ListAdapter.kt
@@ -14,8 +14,8 @@ class ListAdapter(val context: Context, val items: ArrayList<FormData>) :
     RecyclerView.Adapter<ListAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val formId: TextView = view.form_id
-        val formType: TextView = view.form_type
+        // val formId: TextView = view.form_id
+        // val formType: TextView = view.form_type
         val formDate: TextView = view.form_date
         val formDescription: TextView = view.form_description
         val formAmount: TextView = view.form_amount
@@ -35,8 +35,8 @@ class ListAdapter(val context: Context, val items: ArrayList<FormData>) :
 
         val item = items[position]
 
-        holder.formId.text = item.id
-        holder.formType.text = item.type
+        // holder.formId.text = item.id
+        // holder.formType.text = item.type
         holder.formDate.text = item.date
         holder.formDescription.text = item.description
         holder.formAmount.text = item.amount
