f9b96823b4b09d45b2b8a0313d1144230de6d5b9
Nico Kaschowitz <nico.kaschowitz@student.tugraz.at>
2021-06-09 11:30:49 +0200

    SAUP - 11 income tags saved in database [KN, WC]

--- SaveUp/app/src/main/java/com/example/saveup/ui/form/IncomeFragment.kt
+++ SaveUp/app/src/main/java/com/example/saveup/ui/form/IncomeFragment.kt
@@ -18,7 +18,9 @@ import kotlinx.android.synthetic.main.fragment_income.view.*
 
 
 class IncomeFragment : Fragment() {
-
+    private var income_check = booleanArrayOf(false, false, false)
+    private val income_tags_array = arrayOf("salary", "gifts", "payback")
+    private val tags_database : MutableList<String> = ArrayList()
     private lateinit var pageViewModel: PageViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,27 +31,35 @@ class IncomeFragment : Fragment() {
     }
 
     private fun showIncomeTags(){
+
+        var count = 0
+        while(count < income_check.size) {
+            income_check[count] = false
+            count++
+        }
+        count = 0
+
         lateinit var income_tags: AlertDialog
-        val income_tags_array = arrayOf("salary", "gifts", "payback")
-        val income_check = booleanArrayOf(false, false, false)
+
         val tags_store = arrayOf("0", "0", "0")
         val builder = AlertDialog.Builder(this.context)
         builder.setTitle("Choose tags")
         builder.setMultiChoiceItems(income_tags_array, income_check){dialog, which, isChecked ->
             income_check[which] = isChecked
-            tags_store[which] = income_tags_array[which]
+
         }
-        var x = 0
-        var y = 0
-        val tags_database = Array(3) { "" }
-        while(x <= income_tags_array.size)
-            if(tags_store[x] != "0")
-                tags_database[y] = tags_store[x]
-                y++
-            x++
+
+
+
 
         builder.setPositiveButton("OK") { _, _ ->
             Toast.makeText(this.context,"Ok.",Toast.LENGTH_SHORT).show()
+            var x = 0
+            while(x < income_tags_array.size){
+                if(income_check[x]){
+                    tags_database.add(income_tags_array[x])
+                }
+                x++}
         }
         income_tags = builder.create()
         income_tags.show()
@@ -76,14 +86,29 @@ class IncomeFragment : Fragment() {
             val account = root.account_input_field_income.text.toString()
             val category = root.category_input_field_income.text.toString()
             val description = root.description_input_field_income.text.toString()
-            /*val tags*/
+            /*val tags */
+            var x = 0
+            var tags = ""
+            while(x < tags_database.size){
+
+                if(x == tags_database.size-1){
+                    tags += tags_database.get(x)
+                }
+                else{
+                    tags += tags_database.get(x) + ","
+
+                }
+                x++
+            }
+            Toast.makeText(this.context, tags, Toast.LENGTH_SHORT).show()
+
 
             val queue = Volley.newRequestQueue(this.context)
             val url = "https://saveup.weisl.cc/userdata"
 
             val requestBody = "type=" + type + "&amount=" + income_amount + "&date=" + date +
                               "&account=" + account + "&category=" + category +
-                              "&description=" + description /*+"&tags=" + tags*/
+                              "&description=" + description + "&tags=" + tags
 
             val stringReq : StringRequest =
                 object : StringRequest(Method.POST, url,
